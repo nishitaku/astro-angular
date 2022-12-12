58b4b939e7df58ee3de506c9976a4e9321ad8e8d
nishitaku <29690066+nishitaku@users.noreply.github.com>
2022-12-12 13:34:30 +0900

    npx astro add @analogjs/astro-angular

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+import analogjsangular from "@analogjs/astro-angular";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [analogjsangular()]
+});
